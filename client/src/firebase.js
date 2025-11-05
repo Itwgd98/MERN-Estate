fadcef2b94fa5b69a2c522fabdc5f54756b84aa8
Gagan Dixit <148959208+Itwgd98@users.noreply.github.com>
2025-11-05 16:27:09 +0530

    Update firebase.js because of settlement of cors error

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,20 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-estate-abb1c.firebaseapp.com",
   projectId: "mern-estate-abb1c",
-  storageBucket: "mern-estate-abb1c.firebasestorage.app",
+  storageBucket: "mern-estate-abb1c.appspot.com", // ✅ fixed line
   messagingSenderId: "438317992324",
   appId: "1:438317992324:web:f32c2f27f83a51f350b5d1",
-  measurementId: "G-432QY2QJ6M"
+  measurementId: "G-432QY2QJ6M",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
