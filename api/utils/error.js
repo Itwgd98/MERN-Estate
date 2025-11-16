0ef0a1d8706f528e0b354be1bec7d2c0d117d346
Itwgd98 <gagandixit925@gmail.com>
2025-11-16 20:16:28 +0530

    Fix: cleaned user routes and error handler

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;
   error.message = message;
   return error;
 };
-=======
-export const errorHandler = (statusCode, message) => {
-  const error = new Error();
-  error.statusCode = statusCode;
-  error.message = message;
-  return error;
-};
->>>>>>> bed2e5e9d950598e9f45175f83ed407444ed0bbe
