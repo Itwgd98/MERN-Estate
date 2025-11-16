1576e1319d974ed12e2930c2c1bcd7079e26525d
Itwgd98 <gagandixit925@gmail.com>
2025-11-16 17:23:34 +0530

    Fix: cleaned vite.config.js merge conflict

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-<<<<<<< HEAD
-        target: 'http://localhost:3000',
-=======
-        target: 'http://localhost:876',
->>>>>>> bed2e5e9d950598e9f45175f83ed407444ed0bbe
+        target: 'http://localhost:3000', // correct backend port
         secure: false,
       },
     },
